a1c95e9c1452b72c6dd722c893e120f524cff021
Egor Vlasov <vlasov05102002@gmail.com>
2020-09-28 21:53:29 +0300

    added structs

--- lib_sys/egor_vlasov.c
+++ lib_sys/egor_vlasov.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+//  libs declaration
 
+#include <stdio.h>
+#include <string.h>
+
+
+//  global variables declaration
 const int INF = 100000000;
 
+
+//  structs declaration
+struct book {
+    char title[60];
+    char author[50];
+    int year;
+    float book_rate;
+};
+
+struct customer {
+    char name[50];
+    int age;
+    float customer_rate;
+};
+
+//  main
 int main()
 {
     
