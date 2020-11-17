04598ab4039336b746baf7227bfac617c3760994
Egor Vlasov <vlasov05102002@gmail.com>
2020-11-17 14:51:29 +0300

    added new structures
    Co-authored-by: Alex Str <alexandrstrizjnev@gmail.com>

---
+++ battle_sim/src/simulator/egor_vlasov/MyWorldController.java
@@ -0,0 +1,40 @@
+import simulator.do_not_change.WorldController;
+import simulator.do_not_change.Symbol;
+import simulator.do_not_change.Passive;
+import simulator.do_not_change.Aggressive;
+import simulator.do_not_change.CapitalCase;
+import simulator.do_not_change.SmallCase;
+
+public class MyWorldController extends WorldController {
+    public void symbolsMove(List<Symbol> symbols) {
+
+    }
+
+    public void symbolsDie(List<Symbol> symbols) {
+
+    }
+
+    public void smallCaseUpgrade(List<SmallCase> symbols) {
+
+    }
+
+    public void capitalCaseJump(List<CapitalCase> symbols) {
+
+    }
+
+    public void passiveEscape(List<Passive> symbols) {
+
+    }
+
+    public void passiveBreed(List<Passive> symbols) {
+
+    }
+
+    public void aggressiveAttackSmart(List<Aggressive> symbols) {
+
+    }
+
+    public String plotWorld() {
+
+    }
+}
--- battle_sim/src/simulator/egor_vlasov/Simulator.java
+++ battle_sim/src/simulator/egor_vlasov/Simulator.java
@@ -1,3 +1,9 @@
+import simulator.do_not_change.WorldController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Simulator {
     public static void main(String[] args) {
         
